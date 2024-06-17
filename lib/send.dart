898a0096c7ff406357b47301a3e227ceff9640a0
sahil-parmar1 <sahilparmarpalanpur@gmail.com>
2024-06-17 11:19:05 +0530

    hello check

--- lib/send.dart
+++ lib/send.dart
@@ -25,6 +25,7 @@ Future<List<String>> _getVillageSuggestions(String query) async {
   // Filter villages based on the query
   List<String> filteredVillages = villages.where((village) => village.toLowerCase().startsWith(query.toLowerCase())).toList();
   print("$filteredVillages");
+  print("hello check");
   return filteredVillages;
 }
 
